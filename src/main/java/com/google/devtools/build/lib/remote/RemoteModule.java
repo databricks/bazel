8760f5e5a67f4099ed235a7b73c73f43f8b62690
Kevin Lin <developer@kevinlin.info>
2021-12-14 11:37:58 -0800

    Remote: Support client TLS authentication for remote caches over HTTP
    Fixes #14397.
    
    This change augments the `SslContext` created for TLS HTTP remote caches to include the client certificate, client key, and optional root CA as specified on the command line with `--tls_client_certificate`, `--tls_client_key`, and `--tls_certificate`, respectively.
    
    The implementation proposes propagating `AuthAndTLSOptions` through `RemoteCacheClientFactory` and `HttpCacheClient` in order to expose the relevant file paths to the logic that creates the SSL context for use by the HTTP remote cache client.
    
    Closes #14398.
    
    PiperOrigin-RevId: 416354592

--- src/main/java/com/google/devtools/build/lib/remote/RemoteModule.java
+++ src/main/java/com/google/devtools/build/lib/remote/RemoteModule.java
@@ -234,6 +234,7 @@ public final class RemoteModule extends BlazeModule {
           RemoteCacheClientFactory.create(
               remoteOptions,
               creds,
+              authAndTlsOptions,
               Preconditions.checkNotNull(env.getWorkingDirectory(), "workingDirectory"),
               digestUtil);
     } catch (IOException e) {

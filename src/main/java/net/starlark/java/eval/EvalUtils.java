542a4e77e746ed407e8f2f9f77e64ea14f33f93a
Fabian Meumertzheim <fabian@meumertzhe.im>
2022-03-01 11:36:47 -0800

    Starlark: Add dict union operators | and |=
    Implements the Starlark spec addition of https://github.com/bazelbuild/starlark/pull/215.
    
    Work towards #12457
    
    Closes #14540.
    
    PiperOrigin-RevId: 431737269

--- src/main/java/net/starlark/java/eval/EvalUtils.java
+++ src/main/java/net/starlark/java/eval/EvalUtils.java
@@ -127,6 +127,11 @@ final class EvalUtils {
             // int | int
             return StarlarkInt.or((StarlarkInt) x, (StarlarkInt) y);
           }
+        } else if (x instanceof Dict) {
+          if (y instanceof Dict) {
+            // dict | dict
+            return Dict.builder().putAll((Dict<?, ?>) x).putAll((Dict<?, ?>) y).build(mu);
+          }
         }
         break;
 

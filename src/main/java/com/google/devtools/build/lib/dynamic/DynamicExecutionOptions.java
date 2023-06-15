a1d89be9112d713674644839c16138b6fe523384
Googler <larsrc@google.com>
2023-06-15 04:54:17 -0700

    Categorize some more old flags.
    PiperOrigin-RevId: 540544984
    Change-Id: I12748200471d962eecd9d971fe955834cdd889c0

--- src/main/java/com/google/devtools/build/lib/dynamic/DynamicExecutionOptions.java
+++ src/main/java/com/google/devtools/build/lib/dynamic/DynamicExecutionOptions.java
@@ -178,7 +178,7 @@ public class DynamicExecutionOptions extends OptionsBase {
 
   @Option(
       name = "experimental_dynamic_ignore_local_signals",
-      documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
+      documentationCategory = OptionDocumentationCategory.BUILD_TIME_OPTIMIZATION,
       converter = SignalListConverter.class,
       effectTags = {OptionEffectTag.EXECUTION},
       defaultValue = "null",

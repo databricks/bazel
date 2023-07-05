2e0eee095ff5f76f1e5a00ee25825e6cb4573335
Alessandro Patti <ale812@yahoo.it>
2023-07-05 10:27:24 +0200

    Revert "Mark the use_toolchain_transition flag and API as deprecated."
    This reverts commit 2d5375c27a4ca43635183bbe0d12849c64a6176e.

--- src/main/java/com/google/devtools/build/lib/analysis/starlark/StarlarkRuleClassFunctions.java
+++ src/main/java/com/google/devtools/build/lib/analysis/starlark/StarlarkRuleClassFunctions.java
@@ -383,8 +383,9 @@ public class StarlarkRuleClassFunctions implements StarlarkRuleFunctionsApi<Arti
         bzlModule != null ? bzlModule.bzlTransitiveDigest() : new byte[0]);
 
     builder.addToolchainTypes(parseToolchainTypes(toolchains, thread));
-    // Always set this.
-    builder.useToolchainTransition(ToolchainTransitionMode.ENABLED);
+    if (useToolchainTransition) {
+      builder.useToolchainTransition(ToolchainTransitionMode.ENABLED);
+    }
 
     if (execGroups != Starlark.NONE) {
       Map<String, ExecGroup> execGroupDict =
@@ -770,7 +771,7 @@ public class StarlarkRuleClassFunctions implements StarlarkRuleFunctionsApi<Arti
         applyToGeneratingRules,
         execCompatibleWith,
         execGroups,
-        /* useToolchainTransition= */ true);
+        useToolchainTransition);
   }
 
   /**

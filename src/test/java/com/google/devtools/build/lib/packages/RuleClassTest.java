ef310c6a57053a57497a7e6fa1d37ab2581cae3c
Alessandro Patti <ale812@yahoo.it>
2023-07-05 10:18:22 +0200

    Revert "Remove Rule and Aspect code that checks use_toolchain_transition."
    This reverts commit 9a6e14182b5b14f1b6ede51cc74ff18ee9851692.

--- src/test/java/com/google/devtools/build/lib/packages/RuleClassTest.java
+++ src/test/java/com/google/devtools/build/lib/packages/RuleClassTest.java
@@ -56,6 +56,7 @@ import com.google.devtools.build.lib.packages.Attribute.ValidityPredicate;
 import com.google.devtools.build.lib.packages.RuleClass.Builder.RuleClassType;
 import com.google.devtools.build.lib.packages.RuleClass.ConfiguredTargetFactory;
 import com.google.devtools.build.lib.packages.RuleClass.ToolchainResolutionMode;
+import com.google.devtools.build.lib.packages.RuleClass.ToolchainTransitionMode;
 import com.google.devtools.build.lib.packages.RuleFactory.BuildLangTypedAttributeValuesMap;
 import com.google.devtools.build.lib.packages.util.PackageLoadingTestCase;
 import com.google.devtools.build.lib.vfs.Path;
@@ -1076,6 +1077,7 @@ public class RuleClassTest extends PackageLoadingTestCase {
         supportsConstraintChecking,
         /*toolchainTypes=*/ ImmutableSet.of(),
         /*useToolchainResolution=*/ ToolchainResolutionMode.ENABLED,
+        /*useToolchainTransition=*/ ToolchainTransitionMode.ENABLED,
         /* executionPlatformConstraints= */ ImmutableSet.of(),
         /* execGroups= */ ImmutableMap.of(),
         OutputFile.Kind.FILE,

83750b38f5f091ec960642282da9e7c166f66c12
Alessandro Patti <ale812@yahoo.it>
2023-07-05 10:18:22 +0200

    Revert "Remove Rule and Aspect code that checks use_toolchain_transition."
    This reverts commit 9a6e14182b5b14f1b6ede51cc74ff18ee9851692.

--- src/main/java/com/google/devtools/build/lib/analysis/PlatformOptions.java
+++ src/main/java/com/google/devtools/build/lib/analysis/PlatformOptions.java
@@ -249,6 +249,7 @@ public class PlatformOptions extends FragmentOptions {
     host.autoConfigureHostPlatform = this.autoConfigureHostPlatform;
     host.useToolchainResolutionForJavaRules = this.useToolchainResolutionForJavaRules;
     host.targetPlatformFallback = this.targetPlatformFallback;
+    host.overrideToolchainTransition = this.overrideToolchainTransition;
     return host;
   }
 

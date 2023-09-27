db4e1b5b7634e95b5bb8ef37c9879b8f29d5a18b
Alessandro Patti <alessandro.patti@databricks.com>
2023-09-27 12:36:32 +0200

    [Databricks][Cgroups] Add cgroups stats

--- src/main/java/com/google/devtools/build/lib/sandbox/cgroups/v1/LegacyMemory.java
+++ src/main/java/com/google/devtools/build/lib/sandbox/cgroups/v1/LegacyMemory.java
@@ -5,6 +5,9 @@ import com.google.devtools.build.lib.sandbox.cgroups.Controller;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class LegacyMemory implements Controller.Memory {
     private final Path path;
@@ -12,6 +15,11 @@ public class LegacyMemory implements Controller.Memory {
     @Override
     public Path getPath() {
         return path;
+    }
+
+    @Override
+    public Path statFile() throws IOException {
+        return path.resolve("memory.stat");
     }
 
     public LegacyMemory(Path path) {
@@ -26,5 +34,20 @@ public class LegacyMemory implements Controller.Memory {
     @Override
     public long getMaxBytes() throws IOException {
         return Long.parseLong(Files.readString(path.resolve("memory.limit_in_bytes")).trim());
+    }
+
+    @Override
+    public long oomKills() throws IOException {
+        for (String line: Files.readAllLines(getPath().resolve("memory.oom_control"))) {
+            if (line.startsWith("oom_kill ")) {
+                return Long.parseLong(line.substring(line.indexOf(" ") + 1));
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public long maxUsage() throws IOException {
+        return Long.parseLong(Files.readString(path.resolve("memory.max_usage_in_bytes")).trim());
     }
 }

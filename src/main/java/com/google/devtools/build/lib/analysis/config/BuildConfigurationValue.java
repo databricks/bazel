6d2c6a947772cf42c08c38a7a9c4e15b9ee5a228
Alessandro Patti <alessandro.patti@databricks.com>
2023-10-16 03:01:28 +0200

    [Cgroups] Extended cgroup support

--- src/main/java/com/google/devtools/build/lib/analysis/config/BuildConfigurationValue.java
+++ src/main/java/com/google/devtools/build/lib/analysis/config/BuildConfigurationValue.java
@@ -901,4 +901,16 @@ public class BuildConfigurationValue implements BuildConfigurationApi, SkyValue 
   public ImmutableSet<String> getReservedActionMnemonics() {
     return reservedActionMnemonics;
   }
+
+  public Map<String, Double> getTestResources(com.google.devtools.build.lib.packages.TestSize size) {
+    if (!buildOptions.contains(com.google.devtools.build.lib.analysis.test.TestConfiguration.TestOptions.class)) {
+      return ImmutableMap.of();
+    }
+    return buildOptions
+        .get(com.google.devtools.build.lib.analysis.test.TestConfiguration.TestOptions.class)
+        .testResources
+        .stream()
+        .collect(ImmutableMap.toImmutableMap(Map.Entry::getKey, e -> e.getValue().get(size)));
+  }
+
 }
